c4002805c637248c5602ef57947eaac1e2c18075
Alexander Pielago <alexanderpielagoquiroz@gmail.com>
2025-09-03 10:58:32 -0500

    feat(ui): Agregue la vista del login con la imagen de la empresa y 2 campos para que el usuario ingrese sus credenciales.

--- app/src/main/java/Vista/LoginViewActivity.java
+++ app/src/main/java/Vista/LoginViewActivity.java
@@ -1,4 +1,4 @@
-package com.example.ecolim_app;
+package Vista;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ecolim_app.R;
+
+public class LoginViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loginform);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
